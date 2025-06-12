53bf4cfa1f15fd7c4f9af1fe4bad86ee997e898d
GaspalCatalin <gaspalcatalin@gmail.com>
2025-06-12 17:36:19 +0100

    Added new features to the page and the form

--- One-Page Website/JS/one_page_website.js
+++ One-Page Website/JS/one_page_website.js
@@ -38,3 +38,55 @@ function showSlides(n) {
     dots[slideIndex-1].className += " active";
     captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+    document.body.classList.remove('fade-out');
+});
+
+function validateForm() {
+    const fnm = document.getElementById("fname").value;
+    const lnm = document.getElementById("lname").value;
+    const eml = document.getElementById("email").value;
+    const phn = document.getElementById("phonenumber").value;
+
+    const fnmErr = document.getElementById("fname-error");
+    const lnmErr = document.getElementById("lname-error");
+    const emlErr = document.getElementById("email-error");
+    const phnErr = document.getElementById("phonenumber-error");
+
+    fnmErr.textContent = "";
+    lnmErr.textContent = "";
+    emlErr.textContent = "";
+    phnErr.textContent = "";
+
+    let isValid = true;
+    if (fnm === "" || /\d/.test(fnm)) {
+        fnmErr.textContent = "Please enter your first name.";
+        isValid = false;
+    }
+    if (lnm === "" || /\d/.test(lnm)) {
+        lnmErr.textContent = "Please enter your last name.";
+        isValid = false;
+    }
+    if (eml === "" || !eml.includes("@") || !eml.includes(".")) {
+        emlErr.textContent = "Please enter your email.";
+        isValid = false;
+    }
+    if (phn === "" || !regExp.test(_mobile)) {
+        phnErr.textContent = "Please enter your phone number.";
+        isValid = false;
+    }
+    if (isValid) {
+        alert("Form submitted successfully!");
+        return true;
+    } else {
+        return false;
+    }
+
+    function resetErrors() {
+        document.getElementById("fname-error").textContent = "";
+        document.getElementById("lname-error").textContent = "";
+        document.getElementById("email-error").textContent = "";
+        document.getElementById("phonenumber-error").textContent = "";
+    }
+}
